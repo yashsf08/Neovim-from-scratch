81d22339db2658f3d9b0ba56977f00771fd6ede1
Yash Sonune <yash.sonune08@gmail.com>
2022-08-15 13:38:15 -0400

    whichkey - Telescope find file in project with ivy bottom

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -89,10 +89,11 @@ local mappings = {
   ["q"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
-  ["f"] = {
-    "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-    "Find files",
-  },
+  -- ["f"] = {
+  --   "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+  --   "Find files",
+  -- },
+  ["f"] = { "<cmd>Telescope find_files theme=ivy<cr>", "Find Files" },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
