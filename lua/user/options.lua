860cdb96fc3087994541271f655b1a7f8410fd03
Yash Sonune <yash.sonune08@gmail.com>
2022-11-13 13:27:50 -0500

    add more options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -6,6 +6,7 @@ local options = {
   conceallevel = 0,                        -- so that `` is visible in markdown files
   fileencoding = "utf-8",                  -- the encoding written to a file
   hlsearch = true,                         -- highlight all matches on previous search pattern
+  incsearch = true,
   ignorecase = true,                       -- ignore case in search patterns
   mouse = "a",                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
@@ -31,11 +32,10 @@ local options = {
   numberwidth = 2,                         -- set number column width to 2 {default 4}
   signcolumn = "no",                       -- always show the sign column, otherwise it would shift the text each time (yes | no | number)
   wrap = false,                            -- display lines as one long line
+  wrapscan = true,                         --[[ Searches wrap around the end of the file ]]
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "JuliaMono:h11",               -- the font used in graphical neovim applications
-  -- guifont = "Cascadia Mono PL:h12",               -- the font used in graphical neovim applications
-  -- guifont = "MonoLisa:h12",               -- the font used in graphical neovim applications
   autoread = true,                         -- autoreload file when something change
   foldmethod = "expr",                     -- Enable code folding with rule as "expression" ("manual" is default value)
   foldexpr = "nvim_treesitter#foldexpr()", -- Using code folding with treesitter
@@ -43,6 +43,9 @@ local options = {
   -- foldclose = "all"                        
   -- nofoldenable = "no",                     -- disable auto code folding on files
   hidden = false,
+  ruler = true,                            --[[ Show the line and column number of the cursor position, separated by a comma.]]
+  linespace = 0,                           --[[ Number of pixel lines inserted between characters. ]]
+  showcmd = true,                          --[[ Show each command executed in the last line of the screen. ]]
 }
 
 vim.opt.shortmess:append "c"
@@ -55,6 +58,7 @@ vim.cmd "set nofen"
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- NOTE: this doesn't seem to work
+
 vim.cmd [[ 
     " trigger `autoread` when files changes on disk
       set autoread
