9c5c961652c3fa6bce99bff3b9e2a0e261363699
Yahs Sonune <yash.sonune08@gmail.com>
2023-12-31 17:54:20 -0500

    fix indent_blankline.lua - change to require 'ibl'

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,5 +1,6 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
+  print("indent_blankline not found")
 	return
 end
 
@@ -60,8 +61,8 @@ vim.wo.colorcolumn = "99999"
 indent_blankline.setup({
 	-- show_end_of_line = true,
 	-- space_char_blankline = " ",
-	show_current_context = true,
-	-- show_current_context_start = true,
+	--[[ show_current_context = true, ]]
+	--[[ show_current_context_start = true, ]]
 	-- char_highlight_list = {
 	--   "IndentBlanklineIndent1",
 	--   "IndentBlanklineIndent2",
