b73c3804d05b78330e06ddf3fba1bfaeb80ddd41
Yash Sonune <yash.sonune08@gmail.com>
2024-11-13 16:37:03 -0800

    init file update

--- init.lua
+++ init.lua
@@ -1,26 +1,23 @@
-require "user.options"                    -- done
-require "user.keymaps"                    -- Perfect
-require "user.plugins"                    -- Perfect
-require "user.ui.neogotham"                 -- Perfect
---[[ require "user.mason-lsp" ]]
-require "user.mason-lsp.temp"
-require "user.cmp"                        -- Perfect
-require "user.telescope"                    -- Perfect
-require "user.treesitter"                   -- Perfect
-require "user.autopairs"                    -- Perfect
-require "user.comment"                    -- Perfect
-require "user.gitsigns2"                    -- Perfect
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
+require("user.options")          -- done
+require("user.keymaps")          -- perfect
+require("user.plugins")          -- perfect
+require("user.ui.darcula")       -- perfect
+require("user.mason_lsp")
+require("user.cmp")              -- perfect
+require("user.telescope")        -- perfect
+require("user.treesitter")       -- perfect
+require("user.autopairs")        -- perfect
+require("user.comment")          -- perfect
+-- require("user.gitsigns")      -- broken
+require("user.nvim-tree")
+require("user.lualine")
+require("user.bufferline")
+require("user.toggleterm")
+require("user.project")
+require("user.impatient")
+require("user.indentline")
+require("user.alpha")
+require("user.whichkey")
+require("user.autocommands")     -- done
+require("user.neotab")           -- done
 -- require "user.diffview"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
-require "user.whichkey"
-require "user.autocommands"
---[[ require "user.codeium" ]]
---[[ require "user.tabout" ]]
---[[ require "user.orgmode" ]]
