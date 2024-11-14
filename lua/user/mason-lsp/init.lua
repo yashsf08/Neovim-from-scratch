eeb714c08a8c2eb04c05e99ce5b132c14f2fb48c
Yash Sonune <yash.sonune08@gmail.com>
2024-11-13 16:21:15 -0800

    mason refactor

--- lua/user/mason-lsp/init.lua
+++
@@ -1,9 +0,0 @@
--- TODO: Needs to leoad all necessary plugins for Mason and nvim-lsp
-require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "clangd", "pyright" },
-}
-
-
-
-
